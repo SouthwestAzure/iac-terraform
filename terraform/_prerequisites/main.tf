7b35962d4552a8383eb2d3b61e753f0a8cd3a086
Chris Wiederspan <chriswiederspan@MININT-UHQU4FJ.redmond.corp.microsoft.com>
2019-02-23 22:31:58 -0700

    Added initial terraform prerequisite files and init AKS files

---
+++ terraform/_prerequisites/main.tf
@@ -0,0 +1,55 @@
+data "azurerm_client_config" "current" { }
+
+variable "resource_group_name" { }
+
+variable "storage_account_name" { }
+
+variable "keyvault_name" { }
+
+variable "keyvault-object-id" { }
+
+variable "location" {
+  default = "westus2"
+}
+
+resource "azurerm_resource_group" "group" {
+  name     = "${var.resource_group_name}"
+  location = "${var.location}"
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "${var.storage_account_name}"
+  resource_group_name      = "${azurerm_resource_group.group.name}"
+  location                 = "${var.location}"
+  account_kind             = "StorageV2"
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+}
+
+resource "azurerm_key_vault" "keyvault" {
+  name                            = "${var.keyvault_name}"
+  resource_group_name             = "${azurerm_resource_group.group.name}"
+  location                        = "${var.location}"
+  tenant_id                       = "${data.azurerm_client_config.current.tenant_id}"
+
+  sku {
+    name = "standard"
+  }
+
+  access_policy {
+    tenant_id = "${data.azurerm_client_config.current.tenant_id}"
+    object_id = "${var.keyvault-object-id}"
+
+    key_permissions = [
+      "create",
+      "delete",
+      "get",
+    ]
+
+    secret_permissions = [
+      "delete",
+      "get",
+      "set",
+    ]
+  }
+}
