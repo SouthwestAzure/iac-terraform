a5504a6a67f4d924958ba09b06865cf9c350529b
Chris Wiederspan <chris@wiederspan.com>
2019-02-25 06:06:02 -0700

    Added backend storage option on resource-group

--- terraform/resource-group/main.tf
+++ terraform/resource-group/main.tf
@@ -4,6 +4,12 @@ variable "location" {
   default = "westus2"
 }
 
+terraform {
+  backend "azurerm" {
+    environment = "public"
+  }
+}
+
 resource "azurerm_resource_group" "group" {
   name     = "${var.resource_group_name}"
   location = "${var.location}"
