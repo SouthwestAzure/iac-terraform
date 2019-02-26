e8cd8bd4ad8abc5914876cdaab67fe0621e7adfa
Chris Wiederspan <chris@wiederspan.com>
2019-02-26 09:44:17 -0700

    Removed the AKS template with http_application_routing_zone_name

--- terraform/kubernetes/main.tf
+++ terraform/kubernetes/main.tf
@@ -50,7 +50,6 @@ resource "azurerm_kubernetes_cluster" "aks" {
   addon_profile {
     http_application_routing {
       enabled = true
-      http_application_routing_zone_name = "mytestingroutingxyz"
     }
   }
 }
